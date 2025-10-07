ec6eb5fc4a21ec48363711405233ae5f567620ff
Prateek Kumar <prateek0426@gmail.com>
2025-10-07 17:19:31 +0530

    implemented user model

--- core/settings.py
+++ core/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
+AUTH_USER_MODEL = 'user.User'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- user/models.py
+++ user/models.py
@@ -1,4 +1,36 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
+  def create_user(self, email, password=None, **extra_fields):
+    if not email:
+      raise ValueError("Email is required..!")
+    email = self.normalize_email(email)
+    user = self.model(email=email, **extra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+  
+  def create_superuser(self, email, password=None, **extra_fields):
+    extra_fields.set_default('is_staff', True)
+    extra_fields.set_default('is_superuser', True)
+    return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  email = models.EmailField(unique=True)
+  profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+  last_seen = models.DateTimeField(null=True, blank=True)
+  password = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
+  
+  USERNAME_FIELD = 'email'
+  
+  objects = UserManager()
+  
+  def __str__(self):
+    return self.email
