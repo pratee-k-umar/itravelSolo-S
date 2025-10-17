94735fbe90adc7fffb3c3e0344b885546bafdd5f
Prateek Kumar <prateek0426@gmail.com>
2025-10-17 16:18:16 +0530

    fix: migration of primary key from int to uuid

--- user/models.py
+++ user/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
@@ -19,6 +20,7 @@ class UserManager(BaseUserManager):
     return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   first_name = models.CharField(max_length=255)
   last_name = models.CharField(max_length=255)
   email = models.EmailField(unique=True)
